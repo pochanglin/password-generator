00673febaf2e9c13dd5999e3aaf51134c2a36cc3
Allen.Lin <allen4353968@gmail.com>
2020-08-22 17:32:07 +0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  password = '';
+
+  onButtonClick(){
+    this.password = 'hi there'; 
+  } 
 }
