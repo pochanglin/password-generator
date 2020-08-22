52fd51126b7da977e382b799d8b051c22a5a94b2
PoChangLin <allen4353968@gmail.com>
2020-08-22 20:38:27 +0800

    adding event handling method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0;
+  isUseLetters = false;
+  isUseNumbers = false;
+  isUseSymbols = false;
   password = '';
 
   onButtonClick(){
-    this.password = 'hi there'; 
-  } 
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if(this.isUseLetters){
+      validChars += letters;
+    }
+
+    if(this.isUseNumbers){
+      validChars += numbers;
+    }
+
+    if(this.isUseSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPwd = '';
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPwd += validChars[index];
+    }
+    this.password = generatedPwd;
+  }
+
+  onChangeUseLetters(){
+    this.isUseLetters = !this.isUseLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.isUseNumbers = !this.isUseNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.isUseSymbols = !this.isUseSymbols;
+  }
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
 }
